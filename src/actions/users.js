9019e77af41d36f00995c8d6505c5fe6dd120d36
Ryan Wessel <rynjwssl@gmail.com>
2018-07-24 10:01:17 -0400

    separate actions into separate files by function

---
+++ src/actions/users.js
@@ -0,0 +1,5 @@
+// This import loads the firebase namespace along with all its type information.
+import firebase from 'firebase/app';
+
+// These imports load individual services into the firebase namespace.
+import 'firebase/auth';
