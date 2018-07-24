f1fb98be68ed52a96b45a0f7bfef509f4550dbad
Ryan Wessel <rynjwssl@gmail.com>
2018-07-24 10:01:32 -0400

    separate reducers into separate files by function

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,59 +2,9 @@ import {
 	combineReducers
 } from 'redux';
 import {
-	SELECT_PORTFOLIO_TYPE,
-	REQUEST_PORTFOLIO_ITEMS,
-	RECEIVE_PORTFOLIO_ITEMS
-} from '../actions';
-
-const initialState = {
-	selected: '',
-	items: {},
-};
-
-function selectedPortfolioType(state = initialState.selected, action) {
-	switch (action.type) {
-	case SELECT_PORTFOLIO_TYPE:
-		return action.portfolioType;
-	default:
-		return state;
-	}
-}
-
-function items(
-	state = {
-		isFetching: false,
-		items: {},
-	},
-	action
-) {
-	switch (action.type) {
-	case REQUEST_PORTFOLIO_ITEMS:
-		return Object.assign({}, state, {
-			isFetching: true,
-		});
-	case RECEIVE_PORTFOLIO_ITEMS:
-		return Object.assign({}, state, {
-			isFetching: false,
-			items: action.items,
-			lastUpdated: action.receivedAt
-		});
-	default:
-		return state;
-	}
-}
-
-function itemsByPortfolioType(state = initialState.items, action) {
-	switch (action.type) {
-	case RECEIVE_PORTFOLIO_ITEMS:
-	case REQUEST_PORTFOLIO_ITEMS:
-		return Object.assign({}, state, {
-			[action.portfolioType]: items(state[action.portfolioType], action)
-		});
-	default:
-		return state;
-	}
-}
+	itemsByPortfolioType,
+	selectedPortfolioType
+} from './portfolio';
 
 const rootReducer = combineReducers({
 	itemsByPortfolioType,
---
+++ src/reducers/portfolio.js
@@ -0,0 +1,54 @@
+import {
+	SELECT_PORTFOLIO_TYPE,
+	REQUEST_PORTFOLIO_ITEMS,
+	RECEIVE_PORTFOLIO_ITEMS
+} from '../actions';
+
+const initialState = {
+	selected: '',
+	items: {},
+};
+
+export function selectedPortfolioType(state = initialState.selected, action) {
+	switch (action.type) {
+	case SELECT_PORTFOLIO_TYPE:
+		return action.portfolioType;
+	default:
+		return state;
+	}
+}
+
+function items(
+	state = {
+		isFetching: false,
+		items: {},
+	},
+	action
+) {
+	switch (action.type) {
+	case REQUEST_PORTFOLIO_ITEMS:
+		return Object.assign({}, state, {
+			isFetching: true,
+		});
+	case RECEIVE_PORTFOLIO_ITEMS:
+		return Object.assign({}, state, {
+			isFetching: false,
+			items: action.items,
+			lastUpdated: action.receivedAt
+		});
+	default:
+		return state;
+	}
+}
+
+export function itemsByPortfolioType(state = initialState.items, action) {
+	switch (action.type) {
+	case RECEIVE_PORTFOLIO_ITEMS:
+	case REQUEST_PORTFOLIO_ITEMS:
+		return Object.assign({}, state, {
+			[action.portfolioType]: items(state[action.portfolioType], action)
+		});
+	default:
+		return state;
+	}
+}
