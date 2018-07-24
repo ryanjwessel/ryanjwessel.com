8e3c5cc2a66d78dee762873b6db4e30330aa3c3f
Ryan Wessel <rynjwssl@gmail.com>
2018-07-24 12:03:38 -0400

    update project to use webpack 4

--- webpack.common.js
+++ webpack.common.js
@@ -17,21 +17,21 @@ module.exports = {
 		path: path.resolve(__dirname, 'dist')
 	},
 	module: {
-    rules: [
-      { test: /\.scss$/,
-        use: [
-          { loader: "style-loader" },
-		  { loader: "css-loader" },
-		  { loader: "sass-loader" }
-        ]
-      },
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-			loader: "babel-loader",
-		}
-      }
-    ]
-  }
+		rules: [
+			{ test: /\.scss$/,
+				use: [
+					{ loader: 'style-loader' },
+					{ loader: 'css-loader' },
+					{ loader: 'sass-loader' }
+				]
+			},
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+				}
+			}
+		]
+	}
 };
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,21 @@
-const webpack = require('webpack');
 const merge = require('webpack-merge');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
-	plugins: [
-		new UglifyJSPlugin(),
-		new webpack.DefinePlugin({
-			'process.env.NODE_ENV': JSON.stringify('production')
-		})
-	],
+	mode: 'production',
+	optimization: {
+		minimizer: [
+			new UglifyJSPlugin({
+				uglifyOptions: {
+					compress: true,
+					ecma: 6,
+					output: {
+						comments: false,
+					},
+					sourceMap: false,
+				}
+			})
+		]
+	},
 });
