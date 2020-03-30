b6cd39bd8c8699f5dce2923bb7cc6244a57b62f6
Ryan Wessel <rwessel@pebblepost.com>
2020-03-30 17:44:32 -0400

    📦 NEW: remove entire project. start over with next.js basic dependencies

--- src/firebase.js
+++
@@ -1,7 +0,0 @@
-// This import loads the firebase namespace along with all its type information.
-import firebase from 'firebase/app';
-import config from './config';
-
-const fire = firebase.initializeApp(config);
-
-export default fire;
