1bb53901767875f3c0d830395643a00bf0eb09d5
Ryan Wessel <ryan@wesselstudios.com>
2020-06-13 10:24:02 -0400

    👌 IMPROVE: Remove fixed footer, replace with social icons at bottom of each page

--- src/components/StyledFooter.js
+++ src/components/StyledFooter.js
@@ -1,54 +1,17 @@
 import styled from 'styled-components';
 
 const StyledFooter = styled.footer`
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  width: 100%;
   height: 4.5rem;
-  padding: 0.5rem 1rem;
+  padding: 0.5rem 0;
   font-size: 0.75rem;
-  background-color: #f3f3f3;
   color: #666;
   display: flex;
-  flex-wrap: wrap;
-  justify-content: space-between;
+  flex-wrap: nowrap;
+  justify-content: center;
   align-items: center;
 
-  .left,
-  .right {
-    display: flex;
-    align-items: center;
-    flex: 1 0 100%;
-
-    & > *:not(:last-child) {
-      margin-right: 0.75rem;
-    }
-  }
-
-  .left {
-    margin-bottom: 0.5rem;
-    overflow: hidden;
-
-    span {
-      overflow: hidden;
-      white-space: nowrap;
-      text-overflow: ellipsis;
-    }
-  }
-
-  .right {
-    justify-content: center;
-  }
-
-  svg {
-    fill: #666;
-    width: 1.5rem;
-    height: 1.5rem;
-  }
-
   a {
+    margin: 0 0.25rem;
     color: #666;
     &.icon-link {
       cursor: pointer;
@@ -60,20 +23,15 @@ const StyledFooter = styled.footer`
     fill: #0451a5;
   }
 
+  svg {
+    fill: #666;
+    width: 1.5rem;
+    height: 1.5rem;
+  }
+
   @media only screen and (min-width: 768px) {
     height: 3rem;
-    flex-wrap: nowrap;
     font-size: 1rem;
-
-    .left,
-    .right {
-      flex-basis: auto;
-      margin: 0;
-    }
-
-    .right {
-      justify-content: flex-end;
-    }
   }
 `;
 
