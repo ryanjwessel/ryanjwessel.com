9dd5575bfb0c70439ce541fa57f79cbebf929bc2
Ryan Wessel <ryan@wesselstudios.com>
2020-07-07 09:51:18 -0400

    Fix code block width on screens > 1280px

--- src/components/_renderers/StyledCodeBlock.js
+++ src/components/_renderers/StyledCodeBlock.js
@@ -4,11 +4,6 @@ const StyledCodeBlock = css`
   div.code-block {
     margin: 0 -1.25rem 1rem;
   }
-  @media only screen and (min-width: 1280px) {
-    div.code-block {
-      margin: 0 -4rem 1rem;
-    }
-  }
 `;
 
 export default StyledCodeBlock;
