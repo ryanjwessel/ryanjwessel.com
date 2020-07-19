26f6ff7733cf01cfc3f5f6332d6a24b45cd1476f
Ryan Wessel <ryan@wesselstudios.com>
2020-07-19 12:29:44 -0400

    👌 IMPROVE: Remove unnecessary svg attributes, update YouTube icon to fit container

--- src/components/_icons/About.js
+++ src/components/_icons/About.js
@@ -1,15 +1,7 @@
 import React from 'react';
 
 const About = () => (
-  <svg
-    className="icon"
-    xmlns="http://www.w3.org/2000/svg"
-    x="0px"
-    y="0px"
-    width="100"
-    height="100"
-    viewBox="0 0 24 24"
-  >
+  <svg className="icon" xmlns="http://www.w3.org/2000/svg">
     <path d="M12,2C6.477,2,2,6.477,2,12s4.477,10,10,10s10-4.477,10-10S17.523,2,12,2z M13,17h-2v-6h2V17z M13,9h-2V7h2V9z"></path>
   </svg>
 );
