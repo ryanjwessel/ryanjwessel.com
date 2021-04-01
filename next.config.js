0ac6e9baba884789101381a954586ea0ad70f21d
Ryan Wessel <rwessel@dataminr.com>
2021-04-01 09:18:51 -0400

    👌 IMPROVE: Upgrade all dependencies to latest versions

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ module.exports = {
   webpack: function (config) {
     config.module.rules.push({
       test: /\.md$/,
-      use: "raw-loader",
+      use: 'raw-loader',
     });
     return config;
   },
